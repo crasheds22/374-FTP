2948e3f131c4335f380936ca392240fbc8716330
thatguywholiveshere <gilbert.hicks1994@gmail.com>
2017-10-29 13:49:23 +0800

    Add files via upload

--- console.c
+++ console.c
@@ -1,7 +1,20 @@
+#pragma warning(disable: 4996)
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
+
+#ifdef _WIN32
+
+inline FILE* popen(const char* command, const char* type) 
+{
+   return _popen(command, type);
+}
+inline void pclose(FILE* file) 
+{ 
+   _pclose(file); 
+}
+
+#endif
 
 int main( int argc, char *argv[] )
 {
@@ -11,35 +24,54 @@ int main( int argc, char *argv[] )
 	char cmdfull[256];
 	char temp[256];
 	char cmdcd[3];
-	char homedirectory[256];
+	char currentdirectory[256];
   	int exit = 1;
-	int nr;
 	int ret_val;
-	
-	getcwd(homedirectory, sizeof(homedirectory));
-	printf("%s\n", homedirectory);
-	strcpy(cmdfull, "");
+	//strcpy(currentdirectory, "/home/ict374/ass2");
+	//getcwd(homedirectory, sizeof(homedirectory));
+	strcpy(cmdfull, "cd && ");	
 
 	do
 	{
 		printf("enter comand:");
 		fgets(command, 256, stdin);
 		
-		nr = strlen(command);
-		if(command[nr - 1] == '\n')
+		if(command[strlen(command) - 1] == '\n')
 		{
-			command[nr - 1] = '\0';
-			--nr;
+			command[strlen(command) - 1] = '\0';
 		}
-
+		printf("%s\n", command);
 		cmdcd[0] = command[0];
 		cmdcd[1] = command[1];
 		cmdcd[2] = '\0';
 
-		if(strcmp(command, "pwd") == 0 || strcmp(command, "dir") == 0)
+		if(strcmp(command, "pwd") == 0)
+		{
+			#ifdef _WIN32
+				strcpy(command, "cd");
+			#endif
+			strcat(strcpy(temp, cmdfull), command);
+			
+			//system(temp);
+			fp = popen(temp, "r");
+			if (fp == NULL) 
+			{
+				printf("Failed to run command\n" );
+			}
+
+			/* Read the output a line at a time - output it. */
+			while (fgets(path, sizeof(path)-1, fp) != NULL) 
+			{
+				printf("%s", path);
+			}
+
+			/* close */
+			pclose(fp);
+
+		}
+		else if(strcmp(command, "dir") == 0)
 		{
 			strcat(strcpy(temp, cmdfull), command);
-			printf("%s\n", temp);
 			//system(temp);
 			fp = popen(temp, "r");
 			if (fp == NULL) 
@@ -59,11 +91,16 @@ int main( int argc, char *argv[] )
 		}
 		else if(strcmp(cmdcd, "cd") == 0 )
 		{	
-			ret_val = system(command);
-			if(ret_val == 0)
-			{
-				strcat(strcpy(cmdfull, command), " && ");
-			}
+			#ifdef _WIN32
+				if (strcmp(command, "cd") == 0)
+				{
+					strcpy(command, "cd %userprofile%");
+					printf("it entered here\n");
+				}
+			#endif
+			strcat(strcpy(temp, cmdfull), command);
+			ret_val = system(temp);
+			
 			//needed for server
 			//fp = popen(command, "r");
 			//if (fp == NULL) 
@@ -74,11 +111,34 @@ int main( int argc, char *argv[] )
 			/* Read the output a line at a time - output it. */
 			//while (fgets(path, sizeof(path)-1, fp) != NULL) 
 			//{
-			//	printf("%s", path);
+			//	send to client
 			//}
 
 			/* close */
 			//pclose(fp);
+
+			if(ret_val == 0)
+			{
+				#ifdef _WIN32
+					strcat(temp, " && cd");
+				#else			
+					strcat(temp, " && pwd");
+				#endif
+				
+				fp = popen(temp, "r");
+				if (fp == NULL) 
+				{
+					printf("Error occured\n" );
+				}
+				fgets(path, sizeof(path)-1, fp);
+				if(path[strlen(path) - 1] == '\n')
+				{
+					path[strlen(path) - 1] = '\0';
+				}
+				strcpy(currentdirectory, path);
+				strcpy(cmdfull, strcat(strcat(strcpy(temp, "cd "), currentdirectory), " && "));
+			}
+			
 		}
 		else if(strcmp(command, "quit") == 0)
 		{
